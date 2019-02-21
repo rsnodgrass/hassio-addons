fc2c9a00438aa466c54c0d41c6b2bf9e89e09766
Ryan Snodgrass <rsnodgrass@gmail.com>
2019-02-20 18:51:01 -0800

    updated Docker build script

--- docker-build.sh
+++ docker-build.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-cd lutron-radiora1
+cd lutron-radiora-classic
 
 # Default build is based on homeassistant/amd64-base:latest
-docker build -t local/lutron-radiora1 .
+docker build -t local/lutron-radiora-classic .
 
 # To specify a different platform, set build-arg to a different base image:
 # homeassistant/armhf-base
@@ -11,4 +11,4 @@ docker build -t local/lutron-radiora1 .
 # homeassistant/aarch64-base
 # homeassistant/i386-base
 
-#docker build --build-arg BUILD_FROM="homeassistant/amd64-base:latest" -t local/lutron-radiora1 .
+#docker build --build-arg BUILD_FROM="homeassistant/amd64-base:latest" -t local/lutron-radiora-classic .
