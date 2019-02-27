d9ecb3d201ca8cfc90d1ca8a53520c50c3072e98
Ryan Snodgrass <rsnodgrass@gmail.com>
2019-02-27 11:59:02 -0800

    updated docker build

--- docker-build.sh
+++ docker-build.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
 cd radiora-classic-bridge
-docker build -t local/radiora-classic-bridge .
+docker build -t radiora-classic-bridge .
+
+# docker run radiora-classic-bridge
 
 # To specify a different platform, set build-arg to a different base image:
+#   docker build --build-arg BUILD_FROM="homeassistant/amd64-base:latest" -t radiora-classic-bridge .
+#
 # homeassistant/amd64-base (default)
 # homeassistant/armhf-base
 # homeassistant/aarch64-base
 # homeassistant/i386-base
-
-#docker build --build-arg BUILD_FROM="homeassistant/amd64-base:latest" -t local/radiora-classic-bridge .
