ca3345155fe4edf1413177988fc7bdce4960e13c
Ryan Snodgrass <rsnodgrass@gmail.com>
2019-07-18 13:39:02 -0700

    support logging yaml config

--- xantech-serial-bridge/bridge/__init__.py
+++ xantech-serial-bridge/bridge/__init__.py
@@ -1,6 +1,25 @@
 #import os
+import os
+import yaml
 import logging.config
 
-#logging_conf_path = os.path.normpath(os.path.join(os.path.dirname(__file__), 'logging.conf'))
-#logging.config.fileConfig(logging_conf_path)
-logging.config.fileConfig("logging.conf")
+def setup_logging(
+    default_path='logging.yaml',
+    default_level=logging.INFO,
+    env_key='LOG_CONFIG'
+):
+    """Setup logging co>nfiguration
+
+    """
+    path = default_path
+    value = os.getenv(env_key, None)
+    if value:
+        path = value
+    if os.path.exists(path):
+        with open(path, 'rt') as f:
+            config = yaml.safe_load(f.read())
+        logging.config.dictConfig(config)
+    else:
+        logging.basicConfig(level=default_level)
+
+setup_logging()
