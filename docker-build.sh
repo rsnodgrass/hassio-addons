b3ef49977306216740b1e6463ea65ab0ecc70c67
Ryan Snodgrass <rsnodgrass@gmail.com>
2019-07-30 09:05:55 -0700

    docker build a list of components

--- docker-build.sh
+++ docker-build.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 
-#export ADDON=radiora-classic-bridge
-export ADDON=rs485-pool-controller
-
-pushd $ADDON
-docker build --build-arg BUILD_FROM="python:3.6-alpine" -t $ADDON .
-popd
-
-#docker build --build-arg BUILD_FROM="hassioaddons/base:2.3.1" -t $ADDON .
+for ADDON in virtual-ip2sl xantech-serial-bridge radiora-classic-bridge
+do
+    pushd $ADDON
+    #docker build --build-arg BUILD_FROM="hassioaddons/base:2.3.1" -t $ADDON .
+    docker build --build-arg BUILD_FROM="python:3.6-alpine" -t $ADDON .
+    popd
 
 # To specify a different Home Assistant platform, set build-arg to a different base image:
 #   docker build --build-arg BUILD_FROM="homeassistant/amd64-base:latest" -t $ADDON .
