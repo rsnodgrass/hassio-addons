a934cedb4b1946e795ad554a8a36c5eaf23c4c88
Ryan Snodgrass <rsnodgrass@gmail.com>
2019-08-08 22:39:05 -0700

    added dump of yaml to ease seeing setup issues

--- virtual-ip2sl-addon/run.sh
+++ virtual-ip2sl-addon/run.sh
@@ -6,5 +6,6 @@ HASS_CONFIG_PATH=/data/options.json
 # convert Hass.io JSON based config to yaml
 declare -x IP2SL_CONFIG=/data/options.yaml
 yq r $HASS_CONFIG_PATH > $IP2SL_CONFIG
+cat $IP2SL_CONFIG
 
 python3 ip2sl
