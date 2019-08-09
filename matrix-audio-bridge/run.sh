19b52583ccce92795cc4873aacebcc658aea7a88
Ryan Snodgrass <rsnodgrass@gmail.com>
2019-08-08 21:09:31 -0700

    translate Hass.io config options to yaml

---
+++ matrix-audio-bridge/run.sh
@@ -0,0 +1,8 @@
+# Run script for use as Hass.io Add-On (passing in config options)
+HASS_CONFIG_PATH=/data/options.json
+
+# convert Hass.io JSON based config to yaml
+declare -x IIP2SL_CONFIG=/data/options.yaml
+yq r $HASS_CONFIG_PATH > $IP2SL_CONFIG
+
+python3 bridge
