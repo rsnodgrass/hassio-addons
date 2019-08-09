c090a545396c92090db8ae2444a9bf35f5b0eb98
Ryan Snodgrass <rsnodgrass@gmail.com>
2019-08-08 23:11:13 -0700

    improved config output

--- virtual-ip2sl-addon/run.sh
+++ virtual-ip2sl-addon/run.sh
@@ -2,11 +2,15 @@
 
 # Run script for use as Hass.io Add-On (passing in config options)
 HASS_CONFIG_PATH=/data/options.json
+
+echo "Home Assistant config:"
 cat $HASS_CONFIG_PATH
 
 # convert Hass.io JSON based config to yaml
 declare -x IP2SL_CONFIG=/data/options.yaml
 yq -y . $HASS_CONFIG_PATH > $IP2SL_CONFIG
+echo ""
+echo "YAML converted config:"
 cat $IP2SL_CONFIG
 
 python3 ip2sl
