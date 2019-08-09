5b56a1e6e686457136711f96c8d0c561cac74bef
Ryan Snodgrass <rsnodgrass@gmail.com>
2019-08-08 20:55:35 -0700

    starting to get run script working for in Hass environment

--- virtual-ip2sl/run.sh
+++ virtual-ip2sl/run.sh
@@ -1,2 +1,9 @@
+# Run script for use as Hass.io Add-On (passing in config options)
+CONFIG_PATH=/data/options.json
+
 #declare -x IP2SL_CONFIG="your-config.yaml"
+#IP2SL_CONFIG="$(jq --raw-output '.config-file' $CONFIG_PATH)"
+
+# FIXME: how to convert JSON based Add-on config to YAML?
+
 python3 ip2sl
