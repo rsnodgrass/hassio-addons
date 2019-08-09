c7499850b7894b9ee3c44c7c3a5e8befcca6f42a
Ryan Snodgrass <rsnodgrass@gmail.com>
2019-08-08 22:45:56 -0700

    fixed yaml conversion

--- matrix-audio-bridge/run.sh
+++ matrix-audio-bridge/run.sh
@@ -2,7 +2,7 @@
 HASS_CONFIG_PATH=/data/options.json
 
 # convert Hass.io JSON based config to yaml
-declare -x IIP2SL_CONFIG=/data/options.yaml
-yq r $HASS_CONFIG_PATH > $IP2SL_CONFIG
+declare -x IP2SL_CONFIG=/data/options.yaml
+yq -y . $HASS_CONFIG_PATH > $IP2SL_CONFIG
 
 python3 bridge
