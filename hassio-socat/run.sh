c0ae17b396260e153e7ce70dc42af0bc71952d74
Ryan Snodgrass <rsnodgrass@gmail.com>
2020-01-11 12:28:53 -0800

    updates to docker env

--- hassio-socat/run.sh
+++ hassio-socat/run.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bashio
 
 CONFIG_PATH=/data/options.json
-SYSTEM_USER=/data/system_user.json
 
 LOGGING=$(bashio::info 'hassio.info.logging' '.logging')
 
@@ -11,15 +10,18 @@ if [ "${LOGGING}" == "debug" ]; then
     # -d... : The most amount of logging
     # -lu   : Timestamp of error messages to microsecond resolution
     # -v    : Writes the transferred data not only to their target streams, but also to stderr
-    LOG_LEVEL_FLAGS = "-d -d -d -d -lu -v"
+    LOG_LEVEL_FLAGS="-d -d -d -d -lu -v"
 elif [ "${LOGGING}" == "info" ]; then
-    LOG_LEVEL_FLAGS = "-d -d -d"
+    LOG_LEVEL_FLAGS="-d -d -d"
 else
-    LOG_LEVEL_FLAGS = "-d"
+    LOG_LEVEL_FLAGS="-d"
 fi
 
 
 bashio::log.warning "socat is not yet implemented"
+
+PTY="$(jq --raw-output '.pty' $CONFIG_PATH)
+
 
 # iterate through each configured post and create
 # socat pty,link=$HOME/dev/ttyV0,waitslave tcp:remoteip:remoteport    # ,fork
