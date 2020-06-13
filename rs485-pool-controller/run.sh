22fae1a2c7406a7dc3299a0fe1ffa9b8ab570b63
Ryan Snodgrass <rsnodgrass@gmail.com>
2020-06-13 16:42:23 -0700

    stub out socat support

--- rs485-pool-controller/run.sh
+++ rs485-pool-controller/run.sh
@@ -6,20 +6,16 @@ MQTT_BROKER_ADDRESS="$(bashio::config 'mqtt_address')"
 MQTT_USERNAME="$(bashio::config 'mqtt_username')"
 MQTT_PASSWORD="$(bashio::config 'mqtt_password')"
 
-# FIXME:
-# - convert HASSIO config.json and apply to nodejs-poolController config style
-# - start MQTT bridge as well
-
-# FIXME: run out of server dir
-
 # support RS485-over-IP remote devices using SOcketCAT, e.g. SOCAT_ADDRESS="TCP4:192.168.1.44:80"
-SOCAT_ADDRESS="$(bashio::config 'socat_address')"
+SOCAT_OPTIONS="$(bashio::config 'socat')"
 if [ ! -z "$SOCAT_OPTIONS" ]
 then
-    socat -d -d - $SOCAT_ADDRESS
-      echo "\$var is NOT empty"
+    # Examples:
+    #   socat -d -d -d -x PTY,raw,ispeed=9600,ospeed=9600,parenb=0,cstopb=1,cs8,link=/dev/ttyRS485-1 tcp:192.168.3.12:10010
+    #   socat -d -d pty,link=/dev/ttyS0,raw tcp:192.168.0.10:8899
+    echo "Starting socat $SOCAT_OPTIONS"
+    socat $SOCAT_OPTIONS &
 fi
-socat
 
-
+cd /app
 npm start $CONFIG_PATH
