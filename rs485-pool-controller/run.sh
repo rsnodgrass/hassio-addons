e2f928c71d6f8867680d771e4b27a729a37d946a
Ryan Snodgrass <rsnodgrass@gmail.com>
2020-06-13 16:04:12 -0700

    misc updates

--- rs485-pool-controller/run.sh
+++ rs485-pool-controller/run.sh
@@ -1,7 +1,7 @@
 # /data/options.json is the Hass.io config block saved into the Docker instance
 CONFIG_PATH=/data/options.json
 
-# inject the HASS.io configured MQTT credentials via environment variables
+# inject the HASS.io user configured MQTT credentials via environment variables
 MQTT_BROKER_ADDRESS="$(bashio::config 'mqtt_address')"
 MQTT_USERNAME="$(bashio::config 'mqtt_username')"
 MQTT_PASSWORD="$(bashio::config 'mqtt_password')"
