9a544b6274f180f3b1be79730a381229e91aff90
Ryan Snodgrass <rsnodgrass@gmail.com>
2020-06-13 16:42:21 -0700

    stub out socat support

--- rs485-pool-controller/run.sh
+++ rs485-pool-controller/run.sh
@@ -12,4 +12,14 @@ MQTT_PASSWORD="$(bashio::config 'mqtt_password')"
 
 # FIXME: run out of server dir
 
+# support RS485-over-IP remote devices using SOcketCAT, e.g. SOCAT_ADDRESS="TCP4:192.168.1.44:80"
+SOCAT_ADDRESS="$(bashio::config 'socat_address')"
+if [ ! -z "$SOCAT_OPTIONS" ]
+then
+    socat -d -d - $SOCAT_ADDRESS
+      echo "\$var is NOT empty"
+fi
+socat
+
+
 npm start $CONFIG_PATH
