ad9294069c5364c772661ab25764867951dcb66f
Ryan Snodgrass <rsnodgrass@gmail.com>
2024-04-14 23:17:30 -0400

    update

--- keepalived/rootfs/usr/bin/run.sh
+++ keepalived/rootfs/usr/bin/run.sh
@@ -25,15 +25,13 @@ export_config() {
 
     export_config TZ
 
-    export KEEPALIVED_VIRTUAL_IP="$(config_get virtual_ip)"
-    export KEEPALIVED_VIRTUAL_MASK="$(config_get virtual_mask)"
-    export KEEPALIVED_VRID="$(config_get router_id)"
-    export KEEPALIVED_INTERFACE="$(config_get interface)"
-
-    # new options (not in pschmitt version)
-    export KEEPALIVED_CHECK_IP="$(config_get check_ip)"
-    export KEEPALIVED_CHECK_PORT="$(config_get check_port)" # e.g. 53
-    export TZ="$(config_get tz)"
+#    export KEEPALIVED_VIRTUAL_IP="$(config_get virtual_ip)"
+#    export KEEPALIVED_VIRTUAL_MASK="$(config_get virtual_mask)"
+#    export KEEPALIVED_VRID="$(config_get router_id)"
+#    export KEEPALIVED_INTERFACE="$(config_get interface)"
+#    export KEEPALIVED_CHECK_IP="$(config_get check_ip)"
+#    export KEEPALIVED_CHECK_PORT="$(config_get check_port)" # e.g. 53
+#    export TZ="$(config_get tz)"
 
     #export KEEPALIVED_PASSWORD="$(config_get password)"
 }
